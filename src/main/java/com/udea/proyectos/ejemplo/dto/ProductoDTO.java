825af0214354b36c2519152ef3adb25b3fdfeab5
MilySierra <mily.sierra@udea.edu.co>
2026-05-03 03:59:19 -0500

    Mejoras

--- src/main/java/com/udea/proyectos/ejemplo/dto/ProductoDTO.java
+++ src/main/java/com/udea/proyectos/ejemplo/dto/ProductoDTO.java
@@ -1,7 +1,11 @@
 package com.udea.proyectos.ejemplo.dto;
 
 import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter; 
 
+@Getter
+@Setter
 public class ProductoDTO {
     private long id;
     private String nombre;
@@ -21,55 +25,5 @@ public class ProductoDTO {
         this.precio = precio;
         this.imagen = imagen;
     }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public BigDecimal getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(BigDecimal precio) {
-        this.precio = precio;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    
 
 }
--- src/main/java/com/udea/proyectos/ejemplo/dto/UsuarioDTO.java
+++ src/main/java/com/udea/proyectos/ejemplo/dto/UsuarioDTO.java
@@ -1,5 +1,10 @@
 package com.udea.proyectos.ejemplo.dto;
 
+import lombok.Getter;
+import lombok.Setter; 
+
+@Getter
+@Setter
 public class UsuarioDTO {
     private long id;
     private String nombre;
@@ -19,39 +24,6 @@ public class UsuarioDTO {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
-        this.contrasena = contrasena;
-    }
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
     
