28c91de603e354ccd9a9bc2eff73c21c66f93dc5
MilySierra <mily.sierra@udea.edu.co>
2026-05-04 14:53:33 -0500

    Fix: sonarcloud suggestion

--- src/main/java/com/udea/proyectos/ejemplo/config/SecurityConfig.java
+++ src/main/java/com/udea/proyectos/ejemplo/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // todo abierto
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) 
             .httpBasic(basic -> basic.disable())
             .formLogin(form -> form.disable());
 
